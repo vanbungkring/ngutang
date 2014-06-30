f4d46c01e43ea4fb8923df986e163234905f6a28
Arie Prasetyo <vanbungkring@Aries-MacBook-Pro.local>
2014-06-30 19:09:14 +0700

    Initial Commit

---
+++ Ngutang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ngutang
+//
+//  Created by Arie Prasetyo on 6/30/14.
+//  Copyright (c) 2014 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
